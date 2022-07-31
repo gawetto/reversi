9b0b145b371fe233386f5d096aafcec2731779c5
gawetto <5627164+gawetto@users.noreply.github.com>
2022-07-31 18:35:17 +0900

    add crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdout;
+
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    Result,
+};
+
+fn main() -> Result<()> {
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Styled text here."),
+        ResetColor
+    )?;
+    Ok(())
 }
