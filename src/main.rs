b508b94ca6028deef4edba618e02399914d5bbbb
gawetto <5627164+gawetto@users.noreply.github.com>
2022-07-31 19:29:06 +0900

    init crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-use std::io::stdout;
-
 use crossterm::{
-    execute,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    Result,
+    cursor::Hide, cursor::Show, event::read, event::Event, event::KeyCode, event::KeyEvent,
+    execute, style::Print, terminal, terminal::EnterAlternateScreen,
+    terminal::LeaveAlternateScreen, Result,
 };
 
 fn main() -> Result<()> {
-    execute!(
-        stdout(),
-        SetForegroundColor(Color::Blue),
-        SetBackgroundColor(Color::Red),
-        Print("Styled text here."),
-        ResetColor
-    )?;
-    Ok(())
+    terminal::enable_raw_mode()?;
+    execute!(std::io::stderr(), Hide, EnterAlternateScreen)?;
+    loop {
+        match read()? {
+            Event::Key(KeyEvent {
+                code: KeyCode::Esc, ..
+            }) => break,
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('a'),
+                ..
+            }) => {
+                execute!(std::io::stderr(), Print("a"))?;
+            }
+            _ => continue,
+        }
+    }
+    execute!(std::io::stderr(), Show, LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    return Ok(());
 }
